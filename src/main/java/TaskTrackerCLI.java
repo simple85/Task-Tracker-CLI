fff06a499070f4b0749e983f4b7b6f450e058605
berkekocoglu <kocogluberke@gmail.com>
2025-03-30 04:31:12 +0300

    Fixed typos

--- src/main/java/TaskTrackerCLI.java
+++ src/main/java/TaskTrackerCLI.java
@@ -1,7 +1,7 @@
-public class Main {
+public class TaskTrackerCLI {
     public static void main(String[] args) {
         if(args.length == 0) {
-            System.out.println("Usage: task-cli <command> [arguments]\nType /help to list all commands");
+            System.out.println("Usage: TaskTrackerCLI <command> [arguments]\nType /help to list all commands");
             return;
         }
 
@@ -11,7 +11,7 @@ public class Main {
         switch (command) {
             case "add":
                 if(args.length !=2) {
-                    System.out.println("Command failed.\nCorrect usage: task-cli add [\"enter some task description here\"]");
+                    System.out.println("Command failed.\nCorrect usage: TaskTrackerCLI add [\"enter some task description here\"]");
                     return;
                 }
                 taskManager.addTask(args[1]);
@@ -19,7 +19,7 @@ public class Main {
                 
             case "update":
                 if(args.length !=3) {
-                    System.out.println("Command failed.\nCorrect usage: task-cli update [task-id] [\"enter new task description here\"]");
+                    System.out.println("Command failed.\nCorrect usage: TaskTrackerCLI update [task-id] [\"enter new task description here\"]");
                     return;
                 }
                 taskManager.updateTask(Integer.parseInt(args[1]), args[2]);
@@ -27,7 +27,7 @@ public class Main {
                 
             case "delete":
                 if(args.length !=2) {
-                    System.out.println("Command failed.\nCorrect usage: task-cli delete [task-id]");
+                    System.out.println("Command failed.\nCorrect usage: TaskTrackerCLI delete [task-id]");
                     return;
                 }
                 taskManager.deleteTask(Integer.parseInt(args[1]));
@@ -35,23 +35,23 @@ public class Main {
                 
             case "mark-in-progress":
                 if(args.length!=2) {
-                    System.out.println("Command failed.\nCorrect usage: task-cli mark-in-progress [task-id]");
+                    System.out.println("Command failed.\nCorrect usage: TaskTrackerCLI mark-in-progress [task-id]");
                     return;
                 }
-                taskManager.markStatus(Integer.parseInt(args[1]), args[0]);
+                taskManager.markStatus(Integer.parseInt(args[1]), "in-progress");
                 break;
                 
             case "mark-done":
                 if(args.length!=2) {
-                    System.out.println("Command failed.\nCorrect usage: task-cli mark-done [task-id]");
+                    System.out.println("Command failed.\nCorrect usage: TaskTrackerCLI mark-done [task-id]");
                     return;
                 }
-                taskManager.markStatus(Integer.parseInt(args[1]), args[0]);
+                taskManager.markStatus(Integer.parseInt(args[1]), "done");
                 break;
                 
             case "list":
                 if(args.length>2) {
-                    System.out.println("Command failed.\nCorrect usage: task-cli list\nCorrect usage: task-cli list [task-status]");
+                    System.out.println("Command failed.\nCorrect usage: TaskTrackerCLI list\nCorrect usage: TaskTrackerCLI list [task-status]");
                     return;
                 }
                 else if(args.length==1) {
@@ -63,17 +63,17 @@ public class Main {
                 
             case "/help":
                 if(args.length>1) {
-                    System.out.println("Command failed.\nCorrect usage: task-cli /help");
+                    System.out.println("Command failed.\nCorrect usage: TaskTrackerCLI /help");
                     return;
                 }
                 System.out.println("""
-                        task-cli add ["enter some task description here"] | creates a task with the entered description\s
-                        task-cli update [task-id] ["enter new task description here"] | updates the description of the entered task id\s
-                        task-cli delete [task-id] | deletes the task belonging to entered task id\s
-                        task-cli mark-in-progress [task-id] | updates the status of the task to "in-progress" belonging to entered task id\s
-                        task-cli mark-done [task-id] | updates the status of the task to "done" belonging to entered task id\s
-                        task-cli list or task-cli list all | lists all tasks\s
-                        task-cli list [task-status] | lists tasks that matches with entered task-status""");
+                        \nTaskTrackerCLI add ["enter some task description here"]             | creates a task with the entered description\s
+                        TaskTrackerCLI update [task-id] ["enter new task description here"] | updates the description of the entered task id\s
+                        TaskTrackerCLI delete [task-id]                                     | deletes the task belonging to entered task id\s
+                        TaskTrackerCLI mark-in-progress [task-id]                           | updates the status of the task to "in-progress" belonging to entered task id\s
+                        TaskTrackerCLI mark-done [task-id]                                  | updates the status of the task to "done" belonging to entered task id\s
+                        TaskTrackerCLI list or TaskTrackerCLI list all                      | lists all tasks\s
+                        TaskTrackerCLI list [task-status]                                   | lists tasks that matches with entered task-status\n""");
                 break;
 
             default:
